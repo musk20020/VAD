6d7a91ab9a525e06c4bb7e10c633cea1790cc950
musk20020 <edwin20020@gmail.com>
2022-04-20 16:04:26 +0800

    SNR test function

--- SNR_test.py
+++ SNR_test.py
@@ -38,7 +38,7 @@ def segmental_SNR(clean_path, target_path, sr=16000, window_size=256):
     target, _ = librosa.load( target_path, sr=sr )
     max_SNR = -10
 
-    for i in range(0, 200):
+    for i in range(0, 1):
         # target = target[448:]
 
         clean_ori, _ = librosa.load( clean_path, sr=sr )
@@ -78,55 +78,63 @@ def segmental_SNR_v1(clean_path, degrade_path, sr=16000, window_size=256):
 
 
 if __name__ == '__main__':
-    print("====== test SNR ======")
-    test_data_route = '/Users/musk/dataset/VADTestSet/noisy_stationary/10dB/*'
-    # test_data_route = '/Users/musk/dataset/VADTestSet/enhance_stationary/220126/10dB/*'
-    test_list = [tag for tag in iglob(test_data_route+'.wav')]
-    test_list = np.sort(test_list)
+    # print("====== test SNR ======")
+    # test_data_route = '/Users/musk/dataset/VADTestSet/noisy_stationary/10dB/*'
+    # # test_data_route = '/Users/musk/dataset/VADTestSet/enhance_stationary/220126/10dB/*'
+    # test_list = [tag for tag in iglob(test_data_route+'.wav')]
+    # test_list = np.sort(test_list)
+    #
+    # count = 0
+    # SNR_tmp = 0
+    # for filename in test_list:
+    #     print(filename.split("/")[-1])
+    #     ##############################    data parser    ##############################
+    #     # ######  Aurora4  ######
+    #     # clean_name = filename.split( '_' )[-1].split( '.' )[0]
+    #     # speaker = clean_name[:3]
+    #     # clean_path = '/Volumes/Transcend/Download/aurora4/validate_clean'
+    #     # clean_file = '{}/{}_16k/{}..wav'.format(clean_path, speaker, clean_name)
+    #     # #######################
+    #     #
+    #     # ######  TCC300 split ######
+    #     # clean_name = filename.split( '_' )[-1].split( '.' )[0]
+    #     # speaker = clean_name[:9]
+    #     # # clean_path = '/Volumes/TRANSCEND 1/TCC300/Dev/Female/'
+    #     # clean_path = '/Users/musk/dataset/TCC300_split/Dev/'
+    #     # clean_file = '{}/{}/{}.wav'.format( clean_path, speaker, clean_name )
+    #     # #######################
+    #
+    #     # ######  TCC300 rename  ######
+    #     clean_name = filename.split('/')[-1].split('_')[0]
+    #     speaker = clean_name[:9]
+    #     # clean_path = '/Volumes/TRANSCEND 1/TCC300/Dev/Female/'
+    #     clean_path = '/Users/musk/dataset/TCC300_rename/Dev/'
+    #     clean_file = '{}/{}/{}.wav'.format(clean_path, speaker, clean_name)
+    #     # #######################
+    #
+    #     ######  pixel3 record  ######
+    #     # clean_file = '/Volumes/TRANSCEND 1/TCC300_split/Dev/Female/G03FM0110/G03FM011002004.wav'
+    #     #######################
+    #
+    #
+    #     ##############################    criteria test    ##############################
+    #     ######  SNR test  ######
+    #     # SNR_result, index = segmental_SNR(clean_file, filename)
+    #     # SNR_tmp += SNR_result
+    #     # count += 1
+    #     # print(SNR_result, index)
+    #
+    #     ######  PESQ test  ######
+    #     # pesq(clean_file, filename)
+    #
+    # # print('avg_SNR : {}'.format(SNR_tmp/count))
 
-    count = 0
-    SNR_tmp = 0
-    for filename in test_list:
-        print(filename.split("/")[-1])
-        ##############################    data parser    ##############################
-        # ######  Aurora4  ######
-        # clean_name = filename.split( '_' )[-1].split( '.' )[0]
-        # speaker = clean_name[:3]
-        # clean_path = '/Volumes/Transcend/Download/aurora4/validate_clean'
-        # clean_file = '{}/{}_16k/{}..wav'.format(clean_path, speaker, clean_name)
-        # #######################
-        #
-        # ######  TCC300 split ######
-        # clean_name = filename.split( '_' )[-1].split( '.' )[0]
-        # speaker = clean_name[:9]
-        # # clean_path = '/Volumes/TRANSCEND 1/TCC300/Dev/Female/'
-        # clean_path = '/Users/musk/dataset/TCC300_split/Dev/'
-        # clean_file = '{}/{}/{}.wav'.format( clean_path, speaker, clean_name )
-        # #######################
-
-        # ######  TCC300 rename  ######
-        clean_name = filename.split('/')[-1].split('_')[0]
-        speaker = clean_name[:9]
-        # clean_path = '/Volumes/TRANSCEND 1/TCC300/Dev/Female/'
-        clean_path = '/Users/musk/dataset/TCC300_rename/Dev/'
-        clean_file = '{}/{}/{}.wav'.format(clean_path, speaker, clean_name)
-        # #######################
-
-        ######  pixel3 record  ######
-        # clean_file = '/Volumes/TRANSCEND 1/TCC300_split/Dev/Female/G03FM0110/G03FM011002004.wav'
-        #######################
-
-
-        ##############################    criteria test    ##############################
-        ######  SNR test  ######
-        # SNR_result, index = segmental_SNR(clean_file, filename)
-        # SNR_tmp += SNR_result
-        # count += 1
-        # print(SNR_result, index)
-
-        ######  PESQ test  ######
-        # pesq(clean_file, filename)
-
-    # print('avg_SNR : {}'.format(SNR_tmp/count))
+    clean_file = '/Users/musk/Desktop/0208/clean.wav'
+    # filename = '/Users/musk/Desktop/0208/enhance_ori.wav'
+    # filename = '/Users/musk/Desktop/0208/AWS_enhance.wav'
+    filename = '/Users/musk/Desktop/0208/noisy.wav'
+    pesq(clean_file, filename)
+    # SNR_result, index = segmental_SNR(clean_file, filename)
+    # print(SNR_result, index)
 
 
